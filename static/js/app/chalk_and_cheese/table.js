64301e2e129a997977787eb83a5bdbbf3f1c63a8
Alistair Broomhead <alistair.broomhead@gmail.com>
2015-02-02 14:20:17 +0000

    Starts to implement the table

--- static/js/app/chalk_and_cheese/table.js
+++ static/js/app/chalk_and_cheese/table.js
@@ -29,14 +29,16 @@ angular.module('cc-table', ['cc-utils'])
                 active: true
             };
             self.poll = function (){
-                Http.poll('/tables/'+id+'/updated', function(data){
-                    self.set(data);
+                Http.poll('/table/'+id+'/updated', function(data){
+                    if (data.change) self.set(data.data);
                 });
             };
 
-            self.set = function(data){};
+            self.set = function(data){
+                self.data = data;
+            };
             self.get = function(){
-                Http.get('/tables/'+id)
+                Http.get('/table/'+id)
                     .success(function(data){
                         self.set(data);
                     }).then(function(){
@@ -63,12 +65,86 @@ angular.module('cc-table', ['cc-utils'])
             template:
                 '<div id="table-{{index}}" class="tab table-tab">' +
                     '<a href="#table-{{index}}">Table [{{index}}]</a>' +
-                    '<div class="view cc-table">' +
-                        '<cc-table-mouse table="table" ng-model="table"' +
-                        ' ng-repeat="(mouseId, mouse) in table.data.mice"' +
-                        ' mouse="mouse"' +
-                        ' ></cc-table-mouse>' +
+                    '<div class="view cc-table" ng-model="table">' +
+                        '<div>{{table}}</div><br>' +
+                        '<cc-table-mouse ng-repeat="(_id, mouse) in table.data.mice">' +
+                        '</cc-table-mouse>' +
                     '</div>' +
                 '</div>'
         };
-    }]);
+    }])
+    .directive('ccTableMouse', ['Player', function(Player){
+        return {
+            restrict: 'E',
+            replace: true,
+            link: function($scope){
+                $scope.is_turn = function(){
+                    return $scope.table.data.turn == $scope.mouse.uid;
+                };
+                $scope.turnClass = function(){
+                    return $scope.is_turn()? " turn": "";
+                };
+                $scope.is_player = Player.is($scope.mouse);
+
+            },
+            template:
+                '<div ng-model="mouse">' +
+                    '<cc-table-player ng-if="is_player"></cc-table-player>' +
+                    '<div class="mouse{{turnClass()}}" ng-if="!is_player">' +
+                        '<span class="name"> {{mouse.name}}: </span>' +
+                        '<span> Points: {{mouse.points}} </span> | ' +
+                        '<span> Hand: {{mouse.hand}} cards </span> | ' +
+                        '<span> Stack: {{mouse.stack}} cards </span>' +
+                    '</div>' +
+                    '<br>' +
+                '</div>'
+        };
+    }])
+    .directive('ccTablePlayer', function(){
+        return {
+            restrict: 'E',
+            replace: true,
+            link: function($scope){
+                $scope.never = function(){
+                    return false;
+                };
+            },
+            template:
+                '<div class="player{{turnClass()}}">' +
+                    '<div class="name"> {{mouse.name}}: </div>' +
+                    '<div> Points: {{mouse.points}} </div>' +
+                    '<div>' +
+                        'Hand: ' +
+                        '<cc-table-card table="table" card="card"' +
+                                      ' ng-repeat="card in mouse.hand track by $index"' +
+                                      ' enabled="isTurn"></cc-table-card>'+
+                    '</div>' +
+                    '<div>' +
+                        'Stack:' +
+                        '<cc-table-card table="table" card="card"' +
+                                      ' ng-repeat="card in mouse.stack track by $index"' +
+                                      ' enabled="never"></cc-table-card>'+
+                    '</div>' +
+                '</div>'
+        };
+    })
+    .directive('ccTableCard', function(){
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                table: '=',
+                card: '=',
+                enabled: '='
+            },
+            link: function($scope){
+                $scope.place = function(){
+                    $scope.table.place($scope.card);
+                };
+            },
+            template:
+                '<button ng-click="place()" ng-disable="!enabled()">' +
+                    '{{card}}' +
+                '</button>'
+        };
+    });
